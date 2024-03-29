2f900ad5296286850e5a86d8060f6dabd1871f2c
何宇 <3126905039@qq.com>
2024-03-29 21:50:14 +0800

    使用vuex来存储登录信息

--- web/src/component/the-header.vue
+++ web/src/component/the-header.vue
@@ -1,22 +1,27 @@
 <template>
-  <a-layout-header class="header">
-    <div class="logo" />
-    <a-menu
-        v-model:selectedKeys="selectedKeys1"
-        theme="dark"
-        mode="horizontal"
-        :style="{ lineHeight: '64px' }"
-    >
-      <a-menu-item key="1">nav 111</a-menu-item>
-      <a-menu-item key="2">nav 2</a-menu-item>
-      <a-menu-item key="3">nav 3</a-menu-item>
-    </a-menu>
+  <a-layout-header class="header" style="display: flex;;">
+    <div class="logo" style="flex: .8"/>
+    <div style="flex: 5">
+      <!--  -->
+    </div>
+    <div style="flex: 2;color: white;font-weight: 400;font-size: 17px;display: flex">
+      <div style="flex: 2">
+        <span style="white-space: nowrap;text-overflow: ellipsis">你好! {{member.mobile}}</span>
+      </div>
+      <div style="flex: 2">
+        <router-link to="/login">退出系统</router-link>
+      </div>
+
+    </div>
   </a-layout-header>
 </template>
 
 <script setup>
-import {ref} from "vue";
-const selectedKeys1 = ref(['1'])
+// import {ref} from "vue";
+import store from "@/store";
+// const selectedKeys1 = ref(['1'])
+let member = store.state.a.member
+console.log(member.mobile)
 </script>
 
 <style scoped>
