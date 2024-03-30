3e94aeb7a7a3ff23a64f6d375cb171bdae77a73f
何宇 <3126905039@qq.com>
2024-03-30 14:07:05 +0800

    为axios新增拦截配置,在q请求头中带上token信息。并判断后端返回码是否为404

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,4 +1,6 @@
 import {createRouter, createWebHistory} from 'vue-router'
+import {notification} from "ant-design-vue";
+import store from "@/store";
 
 
 const routes = [
@@ -10,20 +12,29 @@ const routes = [
     {
         path: '/index',
         name: 'Index',
-
+        meta: {requiresAuth: true}, // 添加该字段，表示需要认证
         component: () => import('../views/MainView.vue'),
-        children: [
-            {
-                path: '/index/home',
-                name: 'Home',
-                component: () => import('../views/LoginView.vue')
-            }
-
-        ]
     }]
 const router = createRouter({
     history: createWebHistory(),
     routes
 })
+router.beforeEach((to, from, next) => {
+    if (to.matched.some((item) => {
+        return item.meta.requiresAuth;
+    })) {
+        let token = store.state.member?.token;
+        console.log("校验开始")
+        if (!token) {
+            notification.error({description:"未登录或token已过期，请重新登录"})
+            next('/login');
+        } else {
+            next()
+        }
+    } else {
+        next();
+    }
+})
+
 
 export default router
